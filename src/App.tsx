bf3bbe78ed0419137679a0f37c789ce6b0fe6c74
Pham Quang Ha <phamha220203@gmail.com>
2025-12-17 09:42:25 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div className='container bg-red-500 mx-auto'>
+        <h1>Hello taiwind</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
